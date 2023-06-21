506cd39497202989d56ec68330fc38b81e92ca79
Sandeep-Luhaniwal <sandeepluh0001@gmail.com>
2023-06-21 12:59:56 +0530

    page Merge

--- assets/js/style.js
+++ assets/js/style.js
@@ -37,17 +37,63 @@ prevButton.addEventListener('click', () => {
 
 
 
+
+
+$('.slider_1').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: $(".right_arrow"),
+    nextArrow: $(".left_arrow"),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+
+        {
+            breakpoint: 400,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
+
+
+
 // slider
 
 $('.slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 0,
-    speed: 2000,
-    arrows: true,
-    pauseOnHover: true,
     cssEase: 'linear',
     prevArrow: $(".r_arrow"),
     nextArrow: $(".l_arrow"),
@@ -85,4 +131,3 @@ $('.slider').slick({
 
 
 });
-
