470e95a67e9f1ec7441e14d3a3ae89d2425cd26b
Sandeep-Luhaniwal <sandeepluh0001@gmail.com>
2023-06-22 11:13:33 +0530

    preloader section add

--- assets/js/style.js
+++ assets/js/style.js
@@ -136,3 +136,29 @@ $('.slider').slick({
 // ************ AOS Animation ************
 AOS.init();
 once: true;
+
+
+// ************ Back to top ************
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+// ************Preloder*************
+setTimeout(() => {
+    const box = document.getElementById("box");
+
+    box.style.display = "none";
+
+    document.body.classList.remove("overflow-hidden");
+}, 3000);
